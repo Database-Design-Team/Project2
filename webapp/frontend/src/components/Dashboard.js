10dbffa5b3eae76bd041ad33d54b35a8e65fb80d
Ryan Torrecampo <ryantorrecampo@Ryans-MacBook-Pro.local>
2020-04-14 16:39:19 -0500

    Added sidebar to swap components in dashboard page

--- webapp/frontend/src/components/Dashboard.js
+++ webapp/frontend/src/components/Dashboard.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
+import SideNav, { NavItem } from "@trendmicro/react-sidenav";
+
+import Feed from "./Feed";
+import Home from "./Home";
+import Songs from "./Songs";
+import Playlists from "./Playlists";
+import MusicPlayer from "./MusicPlayer";
 import "./Dashboard.scss";
 
 const Dashboard = props => {
@@ -8,13 +15,28 @@ const Dashboard = props => {
   const handleSignOut = () => {
     history.push(`/`);
   };
+
+  const components = {
+    Home: Home,
+    Feed: Feed,
+    Songs: Songs,
+    Playlists: Playlists
+  };
+
+  const [page, changePage] = useState("Home");
+
+  function DynamicComponent(props) {
+    const SelectPage = components[props.page];
+    return <SelectPage />;
+  }
+
   return (
     <div className="dashboard-container">
       <div className="header-container">
         <div className="home-container">Coog Music</div>
         <div className="search-container">
           <input type="text" placeholder="Search" />
-          <i class="fa fa-search"></i>
+          <i className="fa fa-search"></i>
         </div>
         <div className="btn-group">
           <div>
@@ -24,8 +46,32 @@ const Dashboard = props => {
           </div>
         </div>
       </div>
+      <div className="sidebar-container">
+        <SideNav
+          onSelect={selected => {
+            changePage(selected);
+          }}
+        >
+          <SideNav.Nav className="sidenav-container" defaultSelected="home">
+            <NavItem eventKey="Home">Home</NavItem>
+            <NavItem eventKey="Feed">Feed</NavItem>
+            <NavItem eventKey="Songs">Songs</NavItem>
+            <NavItem eventKey="Playlists">Playlists</NavItem>
+          </SideNav.Nav>
+        </SideNav>
+      </div>
       <div className="dashboard-body-container">
-        <h1>Welcome {location.state.login_name}!</h1>
+        {/* <h1>Welcome {location.state.login_name}!</h1> */}
+
+        <div className="home-content-container">
+          <h1>{page}</h1>
+          <div className="content-body-container">
+            <DynamicComponent page={page} />
+          </div>
+        </div>
+      </div>
+      <div className="player-container">
+        <MusicPlayer />
       </div>
     </div>
   );
---
+++ webapp/frontend/src/components/Playlists.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Playlists = props => {
+  return <div></div>;
+};
+
+export default Playlists;
