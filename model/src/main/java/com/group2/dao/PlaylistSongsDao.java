fcc66bcaa3b7f366068ef3c5c69ac296647c8c5f
Timothy Chandler <24783526+Ernaldis@users.noreply.github.com>
2020-04-18 20:37:25 -0500

    Added controller and dao for playlist_songs

---
+++ model/src/main/java/com/group2/dao/PlaylistSongsDao.java
@@ -0,0 +1,10 @@
+package com.group2.dao;
+
+/**
+ * @author timothy
+ * @version 1.0
+ * @since 4/18/20
+ * inside the package - com.group2.dao
+ */
+public class PlaylistSongsDao {
+}
---
+++ webapp/src/main/java/com/group2/webapp/controllers/PlaylistSongsController.java
@@ -0,0 +1,10 @@
+package com.group2.webapp.controllers;
+
+/**
+ * @author Timothy Chandler
+ * @version 1.0
+ * @since 4/18/20
+ * inside the package - com.group2.webapp.controllers
+ */
+public class PlaylistSongsController {
+}
