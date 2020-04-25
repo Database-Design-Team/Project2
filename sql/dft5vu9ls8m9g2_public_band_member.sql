1a7b5269d2158bebfb2d7a31ad38413fed57e989
Timothy Chandler <24783526+Ernaldis@users.noreply.github.com>
2020-04-24 20:21:58 -0500

    added SQL dump

---
+++ sql/dft5vu9ls8m9g2_public_band_member.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.band_member (member_name, band_id) VALUES ('eskettit', 35);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('eskettit', 35);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('ryan', 9);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('eskettit', 15);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('dielit', 15);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('laflame', 15);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('ryan', 15);
+INSERT INTO public.band_member (member_name, band_id) VALUES ('ryan', 12);
